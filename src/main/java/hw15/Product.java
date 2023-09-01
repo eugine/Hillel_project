88dec7d8895724c93cc188d892d8b3bf838ebaf2
Iryna Perepada <irina.pe@voicespin.com>
2023-09-01 09:52:04 +0300

    maded changes

--- src/main/java/hw15/Product.java
+++ src/main/java/hw15/Product.java
@@ -1,4 +1,4 @@
-package hw1;
+package hw15;
 import java.time.LocalDate;
 
 public class Product {
--- src/main/java/hw15/ProductGenerator.java
+++ src/main/java/hw15/ProductGenerator.java
@@ -1,4 +1,4 @@
-package hw1;
+package hw15;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Random;
